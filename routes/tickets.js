abcbef95753c3f32b8ee44267332fd184f480ca6
Rrezon Ahmeti <rrezon.uni@outlook.com>
2022-01-11 17:20:07 +0100

    qrcode

--- routes/tickets.js
+++ routes/tickets.js
@@ -1,6 +1,23 @@
+const { v4: uuidv4 } = require('uuid');
+const qrcodegen = require('../services/qr-generator')
+
 const express = require('express')
 const router = express.Router();
 const db = require('../config/db');
+
+router.get('/verify', (req,res) => {
+    // returns a single ticket
+        db.query('EXEC VerifyTicket :Uuid',
+        {replacements: { Uuid: req.query.uuid }, logging: console.log})
+            .then(data => {
+                res.json({msg: valid});
+            })
+            .catch(err => {
+                console.log(err)
+                res.statusMessage = "Not valid!";
+                res.status(404).end();
+            })
+    })
 
 router.get('/single', (req,res) => {
 // returns a single ticket
@@ -46,10 +63,13 @@ router.get('/search', (req,res) => {
 router.post('/create', (req, res) => {
 // creates a new ticket
 
-    db.query('EXEC CreateTicket :PasangerId, :FlightId, :SeatId, :BagageId, :Price', 
-    {replacements: { PasangerId: req.body.pasangerId, FlightId: req.body.flightId, SeatId: req.body.seatId, BagageId: req.body.bagageId, Price: req.body.price }, logging: console.log})
+    const uuid = uuidv4();
+    const data = `localhost:8000/tickets/verify?uuid=${uuid}`;
+    console.log(data)
+    db.query('EXEC CreateTicket :PasangerId, :FlightId, :SeatId, :BagageId, :Price, :Uuid', 
+    {replacements: { PasangerId: req.body.pasangerId, FlightId: req.body.flightId, SeatId: req.body.seatId, BagageId: req.body.bagageId, Price: req.body.price, Uuid:uuid }, logging: console.log})
         .then(data => {
-            res.json({msg: 'Success'});
+            qrcodegen(data, req, res)
         })
         .catch(err => {
             console.log(err)
--- services/qr-generator.js
+++ services/qr-generator.js
@@ -1,6 +1,6 @@
 const QRCode = require('qrcode');
 
-    let data = req.query.data;
+    //let data = req.query.data;
     // QRCode.toDataURL(data, (err, src) => {
     //     if(err){
     //         res.json({err: err})
@@ -10,11 +10,15 @@ const QRCode = require('qrcode');
     //         res.json({QRCode: src, urlImage: ImageSaved});
     //     }
     // });
+const qrcodegen = (data,req,res) => {
     let QRName = `QR_${new Date().getTime()}.png`;
     QRCode.toFile(`./qrcode/${QRName}`,data, {color: {dark: '#000',light: '#fff'}}, (err) => {
         if(err){
             console.log(err);
         }
         var ImageSaved = req.protocol + '://' + req.get('host') + '/' + QRName;
-            // res.json({urlImage: ImageSaved});
+        return res.json({urlImage: ImageSaved});
     })
+}
+
+module.exports = qrcodegen;
